0566fdabdf0683752474764872fa08074fba31eb
shaurya srivastav <thehellfire99@shauryas-MacBook-Air.local>
2020-02-05 21:28:20 -0800

    3rd commit

--- script.js
+++ script.js
@@ -9,13 +9,3 @@
 //         document.getElementById("navbar").style.top = "-100px";
 //     }
 // }
-
-$({ Counter: 0 }).animate({
-    Counter: $('.Single').text()
-}, {
-    duration: 1000,
-    easing: 'swing',
-    step: function() {
-        $('.Single').text(Math.ceil(this.Counter));
-    }
-});
