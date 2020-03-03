f8e35a733e0537f5ae85ec548c49c77a07e9adb5
Shaurya Srivastav <Shaurya0904@gmail.com>
2020-03-03 10:08:31 -0800

    easter eggs have been hatced

---
+++ script.js
@@ -0,0 +1,3 @@
+console.log("this website was designed by shaurya srivastav");
+console.log("if you have found this, you are probably trying to code or just goofing off, \n either way nice job");
+console.log("shivam needs to learn how to put semi colons! also the answer to life is 42");
