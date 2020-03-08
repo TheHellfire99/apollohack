d9f646488a0830d45bd015468439aa0b0c8c729b
Shaurya Srivastav <Shaurya0904@gmail.com>
2020-03-08 01:05:56 -0800

    proper functioning faq has been made and media queries have been added

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 console.log("this website was designed by shaurya srivastav");
 console.log("if you have found this, you are probably trying to code or just goofing off, \n either way nice job");
 console.log("shivam needs to learn how to put semi colons! also the answer to life is 42");
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
